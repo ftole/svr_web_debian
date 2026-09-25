be6866cb1ef5af88bfc28511abf0a5f61ede3d2f
Frank <josetoledo@audetc.com>
2026-09-25 00:41:36 -0600

    feat: notificaciones tipo toast

--- templates/dashboard/partials/flash.php
+++ templates/dashboard/partials/flash.php
@@ -1,5 +1,11 @@
-<?php foreach ($flashes as $flash): ?>
-    <div class="alert alert-<?= e($flash['type']) ?>">
-        <?= nl2br(e($flash['message'])) ?>
+<?php if (!empty($flashes)): ?>
+    <div class="toast-container">
+        <?php foreach ($flashes as $flash):
+            $type = in_array($flash['type'], ['success', 'error', 'warning', 'info'], true) ? $flash['type'] : 'info'; ?>
+            <div class="toast toast-<?= e($type) ?>" data-toast data-ttl="6000">
+                <div class="toast-body"><?= nl2br(e($flash['message'])) ?></div>
+                <button type="button" class="toast-close" aria-label="Cerrar">&times;</button>
+            </div>
+        <?php endforeach; ?>
     </div>
-<?php endforeach; ?>
+<?php endif; ?>
