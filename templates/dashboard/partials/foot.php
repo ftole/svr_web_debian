be87de47fde30ea272a19a41cf2d12cff1b047ed
Francisco Toledo <cisco_red@outlook.com>
2026-09-26 11:42:48 -0600

    feat(views): enlazar script de material-monitor en pie de pagina

--- templates/dashboard/partials/foot.php
+++ templates/dashboard/partials/foot.php
@@ -1,5 +1,6 @@
 </div>
-<script src="assets/js/app.js"></script>
+<script src="assets/js/material-monitor.js"></script>
 <script src="assets/js/live.js"></script>
+<script src="assets/js/app.js"></script>
 </body>
 </html>
