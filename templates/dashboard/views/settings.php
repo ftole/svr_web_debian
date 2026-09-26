c702c76f080515c8f4f17fe3688edfb6a60ec04a
Francisco Toledo <cisco_red@outlook.com>
2026-09-26 12:24:33 -0600

    fix(views): enlazar descarga de srvctl.conf al endpoint protegido download

--- templates/dashboard/views/settings.php
+++ templates/dashboard/views/settings.php
@@ -30,7 +30,7 @@ require $PANEL_ROOT . '/partials/dev_spec.php';
         </div>
     </div>
     <div class="actions">
-        <a class="btn btn-outline btn-sm" href="/downloads/srvctl.conf" download="srvctl.conf">
+        <a class="btn btn-outline btn-sm" href="/?action=download&type=conf" download="srvctl.conf">
             <svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                 <polyline points="7 10 12 15 17 10"></polyline>
